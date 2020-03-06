e29a4926e4cc8cb56691685a8f865386ecd1c950
Vinicius Dos Santos Paixão <vinicius.ds.paixao@gmail.com>
2020-03-06 16:59:33 -0300

    novas funcionalidades

--- bundle.js
+++ bundle.js
@@ -1,68 +1,77 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var List = /*#__PURE__*/function () {
-  function List() {
-    _classCallCheck(this, List);
-
-    this.data = [];
-  }
-
-  _createClass(List, [{
-    key: "add",
-    value: function add(data) {
-      this.data.push(data);
-      console.log(this.data);
-    }
-  }]);
-
-  return List;
-}();
-
-var TodoList = /*#__PURE__*/function (_List) {
-  _inherits(TodoList, _List);
-
-  function TodoList() {
-    var _this;
-
-    _classCallCheck(this, TodoList);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(TodoList).call(this));
-    _this.usuario = "ronaldinho";
-    return _this;
-  }
-
-  _createClass(TodoList, [{
-    key: "MostraUsuario",
-    value: function MostraUsuario() {
-      console.log(this.usuario);
-    }
-  }]);
-
-  return TodoList;
-}(List);
-
-var minhaLista = new TodoList();
-
-document.querySelector(".newtodo").onclick = function () {
-  minhaLista.add("novo todo");
-};
-
-minhaLista.MostraUsuario();
+// |------------------------------classes-------------------------------------|
+// class List {
+//     constructor() {
+//         this.data = [];
+//     }
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data)
+//     }
+// }
+// class TodoList extends List {
+//     constructor() {
+//         super();
+//         this.usuario = "ronaldinho";
+//     }
+//     MostraUsuario() {
+//         console.log(this.usuario)
+//     }    
+// }
+// const minhaLista = new TodoList();
+// document.querySelector(".newtodo").onclick = function () {
+//     minhaLista.add("novo todo");
+// }
+// minhaLista.MostraUsuario();
+// |------------------------------classes estaticas---------------------------|
+// class Matematica {
+//     static soma (a, b) {
+//         return a  + b ;
+//     }
+// }
+// console.log( Matematica.soma( 1, 2 ));
+// |------------------------------operações em vetores com js es6+-------------------------------|
+// let arr = [1, 2, 3, 4, 5, 6]
+// const newArr = arr.map(function(item, index) {
+//     return item + index ;
+// })
+// console.log(newArr)
+// const  sum = arr.reduce(function(total, next) {
+//     return total + next;
+// })
+// |---------------------iteraçoes do reduce-------------------------|
+//0
+//1
+//1
+//2
+//3
+//3
+//6
+//4
+//10
+//5
+//15
+//6
+//21
+// 1° - total 0   next 1   
+// 2° - total 1   next 2 
+// 3° - total 3   next 3 
+// 4° - total 6   next 4
+// 5° - total 10  next 5
+// 6° - total 15  next 6
+// console.log(sum)
+// const filter = arr.filter(function(item) {
+//     return item % 2 === 0; 
+// })
+// console.log(filter)
+// const find = arr.find(function(item) {
+//     return item === 4; 
+// })
+// console.log(find)
+// |-------------------------------------Arrow functions------------------------------------------|
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
--- main.js
+++ main.js
@@ -1,29 +1,111 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
-    add(data) {
-        this.data.push(data);
-        console.log(this.data)
-    }
-}
-class TodoList extends List {
-    constructor() {
-        super();
 
-        this.usuario = "ronaldinho";
+// |------------------------------classes-------------------------------------|
+
+// class List {
+//     constructor() {
+//         this.data = [];
+//     }
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data)
+//     }
+// }
+// class TodoList extends List {
+//     constructor() {
+//         super();
+
+//         this.usuario = "ronaldinho";
         
-    }
-    MostraUsuario() {
-        console.log(this.usuario)
-    }    
-}
+//     }
+//     MostraUsuario() {
+//         console.log(this.usuario)
+//     }    
+// }
 
-const minhaLista = new TodoList();
+// const minhaLista = new TodoList();
 
-document.querySelector(".newtodo").onclick = function () {
-    minhaLista.add("novo todo");
-}
+// document.querySelector(".newtodo").onclick = function () {
+//     minhaLista.add("novo todo");
+// }
 
-minhaLista.MostraUsuario();
+// minhaLista.MostraUsuario();
 
+
+// |------------------------------classes estaticas---------------------------|
+
+// class Matematica {
+
+//     static soma (a, b) {
+//         return a  + b ;
+//     }
+// }
+
+// console.log( Matematica.soma( 1, 2 ));
+
+// |------------------------------operações em vetores com js es6+-------------------------------|
+
+// let arr = [1, 2, 3, 4, 5, 6]
+
+
+// const newArr = arr.map(function(item, index) {
+//     return item + index ;
+// })
+// console.log(newArr)
+
+// const  sum = arr.reduce(function(total, next) {
+//     return total + next;
+// })
+
+// |---------------------iteraçoes do reduce-------------------------|
+
+//0
+//1
+
+//1
+//2
+
+//3
+//3
+
+//6
+//4
+
+//10
+//5
+
+//15
+//6
+
+//21
+
+// 1° - total 0   next 1   
+// 2° - total 1   next 2 
+// 3° - total 3   next 3 
+// 4° - total 6   next 4
+// 5° - total 10  next 5
+// 6° - total 15  next 6
+
+
+
+// console.log(sum)
+
+// const filter = arr.filter(function(item) {
+//     return item % 2 === 0; 
+// })
+
+// console.log(filter)
+
+// const find = arr.find(function(item) {
+//     return item === 4; 
+// })
+
+// console.log(find)
+
+
+// |-------------------------------------Arrow functions------------------------------------------|
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const newArr = arr.map(item => item * 2)
+
+console.log(newArr)
