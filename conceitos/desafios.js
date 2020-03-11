d3bb11f18a88e59fc1d2d4bc2f50e9615915bb4d
Vinicius Dos Santos Paixão <vinicius.ds.paixao@gmail.com>
2020-03-11 15:10:34 -0300

    desafio completo

--- conceitos/desafios.js
+++ conceitos/desafios.js
@@ -8,33 +8,64 @@
 // true na classe.
 // Agora com suas classes formatadas, adicione um método na classe Usuario chamado isAdmin que
 // retorna se o usuário é administrador ou não baseado na propriedade admin ser true ou não.
-const User1 = new Usuario('email@teste.com', 'senha123');
-const Adm1 = new Admin('email@teste.com', 'senha123');
-console.log(User1.isAdmin()) // false
-console.log(Adm1.isAdmin()) // true
 
-class admin {
-    constructor () {
-       
+class Usuario {
+    constructor (email, senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+    isAdmin () {
+        return this.admin === true;
     }
 }
 
+class Admin extends Usuario {
+    constructor(email, senha) {
+        super(email, senha)
 
+        this.admin === true;
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+// console.log(User1.isAdmin()) // false
+// console.log(Adm1.isAdmin()) // true
+
+// |-------------------------------------------------------------------------------------------|
 
 // 2º exercício
 // A partir do seguinte vetor e utilizando os métodos de array (map, reduce, filter e find):
-// const usuarios = [
-//  { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
-//  { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
-//  { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-// ];
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+];
+// |-------------------------------------------------------------------------------------------|
+
 // 2.1 Utilizando o map
 // Crie uma variável que contenha todas idades dos usuários: [23, 15, 30]
+
+const userIdade = usuarios.map(usuarios => usuarios.idade)
+// console.log(userIdade)
+
+// |-------------------------------------------------------------------------------------------|
+
 // 2.2 Utilizando o filter
 // Crie uma variáveis que tenha apenas os usuários que trabalham na Rocketseat e com mais de 18
 // anos: [{ nome: 'Diego', idade: 23, empresa: 'Rocketseat' }]
+const maior =  usuarios.filter(usuarios => usuarios.empresa === "Rocketseat" && usuarios.idade >= 18)
+// console.log(maior)
+
+// |-------------------------------------------------------------------------------------------|
+
 // 2.3 Utilizando o find
 // Crie uma variável que procura por um usuário que trabalhe na empresa Google: undefined
+const empresa = usuarios.find(usuarios => usuarios.empresa === "google");
+// console.log(empresa)
+
+// |-------------------------------------------------------------------------------------------|
+
 // 2.4 Unindo operações
 // Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem
 // no máximo 50 anos:
@@ -43,94 +74,133 @@ class admin {
 //  { nome: 'Diego', idade: 46, empresa: 'Rocketseat' },
 //  { nome: 'Gabriel', idade: 30, empresa: 'Rocketseat' },
 // ]
+const idade = usuarios.map(usuarios => ({...usuarios, idade: usuarios.idade * 2})).filter(usuarios => usuarios.idade <= 50)
+// console.log(idade)
+
+// |-------------------------------------------------------------------------------------------|
+
 // 3º exercício
 // Converta as funções nos seguintes trechos de código em Arrow Functions:
 // // 3.1
-// const arr = [1, 2, 3, 4, 5];
-// arr.map(function(item) {
-//  return item + 10;
-// });
+const arr = [1, 2, 3, 4, 5];
+arr.map(item => item + 10);
+
+// |-------------------------------------------------------------------------------------------|
+
 // // 3.2
 // // Dica: Utilize uma constante pra function
-// const usuario = { nome: 'Diego', idade: 23 };
-// function mostraIdade(usuario) {
-//  return usuario.idade;
-// }
-// mostraIdade(usuario);
+const usuario = { nome: 'Diego', idade: 23 };
+
+const mostraIdade = usuario => usuario.idade;
+
+mostraIdade(usuario);
+
+// |-------------------------------------------------------------------------------------------|
+
 // // 3.3
 // // Dica: Utilize uma constante pra function
-// const nome = "Diego";
-// const idade = 23;
-// function mostraUsuario(nome = 'Diego', idade = 18) {
-//  return { nome, idade };
-// }
-// mostraUsuario(nome, idade);
-// mostraUsuario(nome);
+const nome = "Diego";
+const idade = 23;
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({nome, idade});
+
+mostraUsuario(nome, idade);
+mostraUsuario(nome);
+
+// |-------------------------------------------------------------------------------------------|
+
 // // 3.4
-// const promise = function() {
-//  return new Promise(function(resolve, reject) {
-//  return resolve();
-//  })
-// }
+const promise = () => new Promise((resolve, reject) => resolve());
+
+// |-------------------------------------------------------------------------------------------|
+
 // 4º exercício
 // 4.1 Desestruturação simples
 // A partir do seguinte objeto:
-// const empresa = {
-//  nome: 'Rocketseat',
-//  endereco: {
-//  cidade: 'Rio do Sul',
-//  estado: 'SC',
-//  }
-// };
+const empresas = {
+    nome: 'Rocketseat',
+    endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
+    }
+};
 // Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
 // fim deve ser possível fazer o seguinte:
+const { nome, endereco: { cidade, estado}} = empresas;
+
 // console.log(nome); // Rocketseat
 // console.log(cidade); // Rio do Sul
 // console.log(estado); // SC
+
+// |-------------------------------------------------------------------------------------------|
+
 // 4.2 Desestruturação em parâmetros
 // Na seguinte função:
-// function mostraInfo(usuario) {
-//  return `${usuario.nome} tem ${usuario.idade} anos.`;
-// }
-// mostraInfo({ nome: 'Diego', idade: 23 })
+
+const  mostraInfo = ({nome, idade}) => `${nome} tem ${idade} anos.`;
+const mostra = mostraInfo({ nome: 'Diego', idade: 23 });
+console.log(mostra);
 // Utilize a desestruturação nos parâmetros da função para buscar o nome e idade do usuário
 // separadamente e a função poder retornar apenas:
 // return `${nome} tem ${idade} anos.`;
+
+// |-------------------------------------------------------------------------------------------|
+
 // 5º Exercício
 // Utilizando o operador de rest/spread (...) realize as seguintes operações:
 // 5.1 Rest
-// A partir do array: const arr = [1, 2, 3, 4, 5, 6], defina uma variável x que recebe a primeira
+// A partir do array:
+const arr = [1, 2, 3, 4, 5, 6] 
+// defina uma variável x que recebe a primeira
 // posição do vetor e outra variável y que recebe todo restante dos dados.
-// console.log(x); // 1
-// console.log(y); // [2, 3, 4, 5, 6]
+const [x, ...y] = arr;
+console.log(x); // 1
+console.log(y); // [2, 3, 4, 5, 6]
+
 // Crie uma função que recebe inúmeros parâmetros e retorna a soma de todos eles:
-// // function soma...
-// console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-// console.log(soma(1, 2)); // 3
+function soma (...params) {
+    return params.reduce((a, b) => a + b)
+}
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+console.log(soma(1, 2)); // 3
+
+// |-------------------------------------------------------------------------------------------|
+
 // 5.2 Spread
 // A partir do objeto e utilizando o operador spread:
-// const usuario = {
-//  nome: 'Diego',
-//  idade: 23,
-//  endereco: {
-//  cidade: 'Rio do Sul',
-//  uf: 'SC',
-//  pais: 'Brasil',
-//  }
-// };
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+    }
+};
 // Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+const usuario23 = {...usuario, nome: "Gabriel"} 
+console.log(usuario23)
+
 // Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
+
+const usuario3 = {...usuario.endereco, cidade:"Lontras"};
+console.log(usuario3)
+
+// |-------------------------------------------------------------------------------------------|
+
 // 6º Exercício
 // Converta o seguinte trecho de código utilizando Template Literals:
-// const usuario = 'Diego';
-// const idade = 23;
-// console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+const usuario = 'Diego';
+const idade = 23;
+console.log(`O usuário ${usuario} possui ${idade} anos`);
+
+// |-------------------------------------------------------------------------------------------|
+
 // 7º exercício
 // Utilize a sintaxe curta de objetos (Object Short Syntax) no seguinte objeto:
-// const nome = 'Diego';
-// const idade = 23;
-// const usuario = {
-//  nome: nome,
-//  idade: idade,
-//  cidade: 'Rio do Sul',
-// };
+const nome = 'Diego';
+const idade = 23;
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul',
+};
