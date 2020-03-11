033f92b59a741f08d815494998c8afa6fd48ef88
Vinicius Dos Santos Paixão <vinicius.ds.paixao@gmail.com>
2020-03-11 16:15:09 -0300

    configuraçoes

--- conceitos/README.md
+++ conceitos/README.md
@@ -1,2 +1,5 @@
 # componentes de classe 
-- **super ()** é passado quando a classe que herda a classe pai  precisa acessar o constructor do pai.  
+- **super ()** é passado quando a classe que herda a classe pai  precisa acessar o constructor do pai. 
+
+# Promisses
+- realiza ações de maneira assincrona a  
--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-alert("funfando")
+// |----------------------then and catch----------------------------------------|
+
+const minhaPromisse = () => new Promise ((resolve, reject) => {
+    setTimeout(() => {resolve("ok")}, 2000);
+});
+
+// minhaPromisse()
+// .then(response => {
+//     console.log(response)
+// })
+// .catch(err => {
+//     console.error(err)
+// });
+
+
+//|---------------------- async await------------------------------------------------------|
+
+async function executaPromise() {
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+}
+
+executaPromise()
