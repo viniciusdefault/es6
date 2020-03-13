49b58ce0ea662e3b46c4d6f82e370b7a77eb8fc5
Vinicius Dos Santos Paixão <vinicius.ds.paixao@gmail.com>
2020-03-13 16:44:25 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // |----------------------then and catch----------------------------------------|
 
-const minhaPromisse = () => new Promise ((resolve, reject) => {
-    setTimeout(() => {resolve("ok")}, 2000);
-});
+// const minhaPromisse = () => new Promise ((resolve, reject) => {
+//     setTimeout(() => {resolve("ok")}, 2000);
+// });
 
 // minhaPromisse()
 // .then(response => {
@@ -15,10 +15,27 @@ const minhaPromisse = () => new Promise ((resolve, reject) => {
 
 //|---------------------- async await------------------------------------------------------|
 
-async function executaPromise() {
-    console.log(await minhaPromisse());
-    console.log(await minhaPromisse());
-    console.log(await minhaPromisse());
-}
+// async function executaPromise() {
+//     console.log(await minhaPromisse());
+//     console.log(await minhaPromisse());
+//     console.log(await minhaPromisse());
+// }
 
-executaPromise()
+// executaPromise()
+
+//|--------------------------------------------axios--------------------------------------------|
+
+import axios from "axios";
+
+class api {
+    static async getUseInfo(username) {
+        try {
+            const  response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        }catch(err) {
+            console.warn('erro na api') 
+        }
+    };
+}
+api.getUseInfo('viniciusdefault')
+
